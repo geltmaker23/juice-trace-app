34306bad74247092e08cba4ad73df5a9ccbade21
geltmaker23 <75448911+geltmaker23@users.noreply.github.com>
2026-02-12 13:22:35 -0500

    Update server.js

--- server.js
+++ server.js
@@ -33,7 +33,9 @@ app.get("/trace", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT;
-app.listen(PORT, () => {
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
+
