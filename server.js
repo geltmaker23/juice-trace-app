a00a8c0692259d35dc98c016006626b6d4babad4
geltmaker23 <75448911+geltmaker23@users.noreply.github.com>
2026-02-12 17:41:15 -0500

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const url = require("url");
 const PORT = process.env.PORT;
 
 const server = http.createServer((req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+res.setHeader("Access-Control-Allow-Methods", "GET");
+res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  
   const parsedUrl = url.parse(req.url, true);
   const pathname = parsedUrl.pathname;
 
