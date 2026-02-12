2fc36c41359cca3c9d39f63fdf53acfb69c4054b
geltmaker23 <75448911+geltmaker23@users.noreply.github.com>
2026-02-12 16:09:23 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
+const express = require("express");
+const { Pool } = require("pg");
+const cors = require("cors");
+
 const app = express();
+app.use(cors());
+app.use(express.json());
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
@@ -49,6 +55,6 @@ app.get("/trace", async (req, res) => {
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, "0.0.0.0", () => {
-  console.log("Server running on port", PORT); });
-
+  console.log("Server running on port", PORT);
+});
 
